a1cc8b1e15655ec02d37de101233e5fc9b02a77e
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2020-08-12 01:52:24 -0400

    Updates

---
+++ precache-manifest.ca22cfacd7b958be79a9cf2a0978ec36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4aea49021ef12789d4ca4c6985909c",
+    "url": "/Leaf-Hunter/index.html"
+  },
+  {
+    "revision": "0adf0bc3c12bad78e813",
+    "url": "/Leaf-Hunter/static/css/main.2f65b8ec.chunk.css"
+  },
+  {
+    "revision": "4d7df7be0e4f7b78f8f2",
+    "url": "/Leaf-Hunter/static/js/2.c9c041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Leaf-Hunter/static/js/2.c9c041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adf0bc3c12bad78e813",
+    "url": "/Leaf-Hunter/static/js/main.5f6b6c07.chunk.js"
+  },
+  {
+    "revision": "225a2ab556c52752ac67",
+    "url": "/Leaf-Hunter/static/js/runtime-main.90f6b773.js"
+  },
+  {
+    "revision": "859f4fc41171d033d94f20a0e21ad2cd",
+    "url": "/Leaf-Hunter/static/media/Leaf_icon_15.859f4fc4.svg"
+  },
+  {
+    "revision": "f52d402cfb24ebf77810ca6065cd7d26",
+    "url": "/Leaf-Hunter/static/media/Leaves-Background-Wallpaper.f52d402c.jpg"
+  }
+]);
