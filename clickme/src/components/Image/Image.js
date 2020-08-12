342b803e974a8ed33442b51a259cc42211a7be3a
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2020-08-12 01:44:48 -0400

    adjusted image urls

--- clickme/src/components/Image/Image.js
+++ clickme/src/components/Image/Image.js
@@ -5,7 +5,7 @@ const Image = props => {
 	return (
 		<div className="card leaf" onClick={() => props.handleClick(props.id)}>
 			<img 
-				src={props.source} 
+				src={process.env.PUBLIC_URL + props.source} 
 				alt={props.name}
 			/>
 		</div>
